2c7d5c1b289e333da771bc07374dbd651ac1923f
Johan Abde <abdejohan@gmail.com>
2023-12-08 10:36:15 +0100

    Add endpoint for fetchin a product by gtin number

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+import products from './products';
 
+const prisma = new PrismaClient();
 // For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
+});
+
+app.get('/api/products/:gtin', async (req: Request, res: Response) => {
+  const { gtin } = req.params;
+  const foundProduct = products.find((product) => product.gtin.includes(gtin));
+  res.json(foundProduct);
 });
 
 app.listen(port, () => {
