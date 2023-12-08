e4892eb9fa2394fb0d07d640286bf28d3406d9f7
Johan Abde <abdejohan@gmail.com>
2023-12-08 10:36:03 +0100

    Create mock products

---
+++ products.ts
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: 'Delicatoboll 6-p',
+    brand: 'DELICATO',
+    category: '10000246 - Pajer/bakverk - söta (kort hållbarhetstid)',
+    countryOfOrigin: 'Sverige',
+    provenanceStatement: 'Förädlad i Sverige',
+    supplierArticleNumber: ['1214', '1363', '940', '1446'],
+    gtin: '07315360061503',
+    lastChangeDate: '2023-06-02',
+    ingredientStatement:
+      'Socker, HAVREGRYN, vegetabilisk olja (palm*, shea, raps), vatten, kokosflingor, kakaopulver, salt, arom (bl.a. mocka), konserveringsmedel (kaliumsorbat), emulgeringsmedel (vegetabiliskt E471, lecitin), färgämne (sockerkulör). Kan innehålla spår av MJÖLKPROTEIN. *Hållbart producerad från certifierade odlare.',
+  },
+];
+
+export default products;
